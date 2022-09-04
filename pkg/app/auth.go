d3b4d4fea19079b7b80032dd38808fae9abd0033
chyroc <chyroc@qq.com>
2022-09-04 21:22:34 +0800

    fix new token

--- pkg/app/auth.go
+++ pkg/app/auth.go
@@ -2,36 +2,35 @@ package app
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/chyroc/dropbox-to-google-photos/pkg/googlephotoclient"
 	"github.com/chyroc/dropbox-to-google-photos/pkg/oauth"
+	"golang.org/x/oauth2"
 )
 
 func (r *App) TryAuth() error {
 	account := r.config.Account
 	r.logger.Infof("Authenticating using token for '%s'", account)
 
-	token, err := r.tokenManager.Get(account)
-	if err != nil {
-		return fmt.Errorf("unable to retrieve token, have you authenticated before?: %w", err)
-	}
-
 	cfg := &oauth.Config{
 		ClientID:     r.config.GooglePhotos.ClientID,
 		ClientSecret: r.config.GooglePhotos.ClientSecret,
 		Logf:         r.logger.Debugf,
 	}
+	token, _ := r.tokenManager.Get(account)
+	var err error
 
-	token, err = oauth.RefreshToken(context.Background(), cfg, token)
+	if token == nil || token.AccessToken == "" {
+		token, err = r.tryNewToken(cfg)
+	} else {
+		token, err = r.tryRefreshToken(cfg, token)
+	}
 	if err != nil {
 		return err
 	}
 
-	r.logger.Donef("Token is valid, expires at %s", token.Expiry)
-
-	if err := r.tokenManager.Put(account, token); err != nil {
-		r.logger.Debugf("Failed to store token into token manager: %s", err)
+	if err := r.tokenManager.Put(r.config.Account, token); err != nil {
+		r.logger.Debugf("[token] Failed to store token into token manager: %s", err)
 	}
 
 	googlePhotoHttpClient, err := oauth.Client(context.Background(), cfg, token)
@@ -40,9 +39,26 @@ func (r *App) TryAuth() error {
 	}
 	r.googlePhotoHttpClient = googlePhotoHttpClient
 	r.googlePhotoClient, err = googlephotoclient.New(r.googlePhotoHttpClient)
+
+	return err
+}
+
+func (r *App) tryNewToken(cfg *oauth.Config) (*oauth2.Token, error) {
+	token, err := oauth.GetToken(context.Background(), cfg)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return token, nil
+}
+
+func (r *App) tryRefreshToken(cfg *oauth.Config, token *oauth2.Token) (*oauth2.Token, error) {
+	token, err := oauth.RefreshToken(context.Background(), cfg, token)
+	if err != nil {
+		return nil, err
+	}
+
+	r.logger.Donef("Token is valid, expires at %s", token.Expiry)
+
+	return token, nil
 }
